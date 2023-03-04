c7bdc29699933d1411b9cce641954f30c7d0ef0e
andrea pagliari <andrea.pagliari12@gmail.com>
2023-03-04 13:48:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 function car_back () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Back, 40)
-    mecanumRobot.Motor(LR.Lower_left, MD.Back, 40)
-    mecanumRobot.Motor(LR.Upper_right, MD.Back, 40)
-    mecanumRobot.Motor(LR.Lower_right, MD.Back, 40)
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 25)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 25)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 25)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 25)
 }
 function car_left () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Back, 60)
-    mecanumRobot.Motor(LR.Lower_left, MD.Back, 60)
-    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 85)
-}
-function car_forward () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 40)
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 20)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 20)
     mecanumRobot.Motor(LR.Upper_right, MD.Forward, 40)
     mecanumRobot.Motor(LR.Lower_right, MD.Forward, 40)
 }
+function car_forward () {
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 25)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 25)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 25)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 25)
+}
 function car_right () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Upper_right, MD.Back, 60)
-    mecanumRobot.Motor(LR.Lower_right, MD.Back, 60)
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 40)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 40)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 20)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 20)
 }
 basic.forever(function () {
     if (mecanumRobot.LineTracking(LT.Left) == 0 && mecanumRobot.LineTracking(LT.Right) == 1) {
