1cb612e304b82a8a68b382b4631243b97f3b9587
andrea pagliari <andrea.pagliari12@gmail.com>
2023-03-04 15:05:29 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ function car_back () {
     mecanumRobot.Motor(LR.Lower_right, MD.Back, 25)
 }
 function car_left () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Back, 20)
-    mecanumRobot.Motor(LR.Lower_left, MD.Back, 20)
-    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 40)
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 50)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 50)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 20)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 20)
 }
 function car_forward () {
     mecanumRobot.Motor(LR.Upper_left, MD.Forward, 25)
@@ -17,10 +17,10 @@ function car_forward () {
     mecanumRobot.Motor(LR.Lower_right, MD.Forward, 25)
 }
 function car_right () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Upper_right, MD.Back, 20)
-    mecanumRobot.Motor(LR.Lower_right, MD.Back, 20)
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 20)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 20)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 50)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 50)
 }
 basic.forever(function () {
     if (mecanumRobot.LineTracking(LT.Left) == 0 && mecanumRobot.LineTracking(LT.Right) == 1) {
